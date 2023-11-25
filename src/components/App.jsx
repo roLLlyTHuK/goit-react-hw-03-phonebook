fd61a47bcda5303413cb8b5f5592d48498598647
roLLlyTHuK <ircvipua@gmail.com>
2023-11-25 11:01:17 +0200

    +add localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,32 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-      contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+      contacts: [],
       filter: '',
     };
   
+  componentDidMount() {
+    let initContacts = localStorage.getItem('contacts');
+    if (!initContacts) {
+      initContacts = [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
+      this.setState({ contacts: initContacts });
+    } else {
+      this.setState({ contacts: JSON.parse(initContacts) });
+    }
+  }
 
+  componentDidUpdate(contacts, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  
+  
   handleSubmit = ({ name, number }) => {
     const { contacts } = this.state;
 
